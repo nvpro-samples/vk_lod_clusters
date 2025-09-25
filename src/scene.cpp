945d6c87f7b85c6239b1eb11515ae8f7dd8e2fc8
Christoph Kubisch <ckubisch@nvidia.com>
2025-09-25 21:00:43 +0200

    use meshoptimizer functions for vertex dedup, thx Arseny Kapoulkine
    Script sync from SHA: 5779f869e09d00e399ad270d8eb719b3a8b26ece

--- src/scene.cpp
+++ src/scene.cpp
@@ -663,128 +663,21 @@ static_assert(std::atomic_uint32_t::is_always_lock_free && sizeof(std::atomic_ui
 
 void Scene::buildGeometryDedupVertices(ProcessingInfo& processingInfo, GeometryStorage& geometry)
 {
-  std::vector<uint32_t> remap(geometry.vertices.size(), ~0);
+  std::vector<uint32_t> remap(geometry.vertices.size());
 
-  size_t hashMapSize = std::max(geometry.vertices.size() / 64, size_t(0xFFFF));
+  size_t uniqueVertices =
+      meshopt_generateVertexRemap(remap.data(), reinterpret_cast<const uint32_t*>(geometry.globalTriangles.data()),
+                                  geometry.globalTriangles.size() * 3, geometry.vertices.data(),
+                                  geometry.vertices.size(), sizeof(glm::vec4));
 
-  // vertex hash key
-  std::vector<size_t> vertexHashKey(geometry.vertices.size());
-  // list of vertices per hash key
-  std::vector<HashVertexRange> hashRanges(hashMapSize, HashVertexRange());
-  std::vector<uint32_t>        hashVertices(geometry.vertices.size());
-
-  // This pass initializes the remap array as `remap[vertexIdx] = vertexIdx`,
-  // while at the same time doing this only for used vertices.
-  // Unused vertices will be left at ~0.
-
-  nvutils::parallel_batches_pooled(
-      geometry.globalTriangles.size(),
-      [&](uint64_t idx, uint32_t threadInnerIdx) {
-        glm::uvec3 tris = geometry.globalTriangles[idx];
-
-        remap[tris.x] = tris.x;
-        remap[tris.y] = tris.y;
-        remap[tris.z] = tris.z;
-      },
-      processingInfo.numInnerThreads);
-
-  nvutils::parallel_batches_pooled(
-      geometry.vertices.size(),
-      [&](uint64_t idx, uint32_t threadInnerIdx) {
-        const glm::vec4& vertex = geometry.vertices[idx];
-
-        size_t hashKey = nvutils::hashVal(vertex.x, vertex.y, vertex.z, vertex.w);
-        hashKey %= hashMapSize;
-
-        vertexHashKey[idx] = hashKey;
-        ((std::atomic_uint32_t&)hashRanges[hashKey].count)++;
-      },
-      processingInfo.numInnerThreads);
-
-  uint32_t offset = 0;
-
-  for(size_t i = 0; i < hashMapSize; i++)
-  {
-    hashRanges[i].offset = offset;
-    offset += hashRanges[i].count;
-    hashRanges[i].count = 0;
-  }
-
-  for(size_t i = 0; i < geometry.vertices.size(); i++)
-  {
-    size_t           key                       = vertexHashKey[i];
-    HashVertexRange& range                     = hashRanges[key];
-    hashVertices[range.offset + range.count++] = uint32_t(i);
-  }
-
-  // actual vertex comparisons
-  nvutils::parallel_batches_pooled(
-      geometry.vertices.size(),
-      [&](uint64_t idx, uint32_t threadInnerIdx) {
-        if(((std::atomic_uint32_t&)remap[idx]).load() == uint32_t(idx))
-        {
-          const glm::vec4&       vertex = geometry.vertices[idx];
-          size_t                 key    = vertexHashKey[idx];
-          const HashVertexRange& range  = hashRanges[key];
-
-          for(uint32_t i = 0; i < range.count; i++)
-          {
-            uint32_t otherVertexIdx = hashVertices[range.offset + i];
-            if(otherVertexIdx <= idx || (((std::atomic_uint32_t&)remap[otherVertexIdx]).load() != otherVertexIdx))
-              continue;
-
-            if(vertex == geometry.vertices[otherVertexIdx])
-            {
-              ((std::atomic_uint32_t&)remap[otherVertexIdx]).store(uint32_t(idx));
-            }
-          }
-        }
-      },
-      processingInfo.numInnerThreads);
-
-  // compaction, create details for new vertices
-
-  uint32_t newVertexOffset = 0;
-  for(size_t i = 0; i < geometry.vertices.size(); i++)
-  {
-    uint32_t otherIdx = remap[i];
-
-    // mapped to ourself, survives
-    if(otherIdx == uint32_t(i))
-    {
-      // store read location of new vertex at offset
-      vertexHashKey[newVertexOffset] = i;
-      // new vertex location for remapping triangle indices
-      remap[i] = newVertexOffset++;
-    }
-    else if(otherIdx != ~0)
-    {
-      // mapping to another vertex (guaranteed handled before)
-      assert(otherIdx < uint32_t(i));
-      remap[i] = remap[otherIdx];
-    }
-  }
-
-  // build new vertices array
-  std::vector<glm::vec4> newVertices(newVertexOffset);
-
-  nvutils::parallel_batches_pooled(
-      newVertices.size(),
-      [&](uint64_t idx, uint32_t threadInnerIdx) { newVertices[idx] = geometry.vertices[vertexHashKey[idx]]; },
-      processingInfo.numInnerThreads);
-
+  std::vector<glm::vec4> newVertices(uniqueVertices);
+  meshopt_remapVertexBuffer(newVertices.data(), geometry.vertices.data(), geometry.vertices.size(), sizeof(glm::vec4),
+                            remap.data());
   geometry.vertices = std::move(newVertices);
 
-  // remap triangle indices
-  nvutils::parallel_batches_pooled(
-      geometry.globalTriangles.size(),
-      [&](uint64_t idx, uint32_t threadInnerIdx) {
-        glm::uvec3& tris = geometry.globalTriangles[idx];
-        tris.x           = remap[tris.x];
-        tris.y           = remap[tris.y];
-        tris.z           = remap[tris.z];
-      },
-      processingInfo.numInnerThreads);
+  meshopt_remapIndexBuffer(reinterpret_cast<uint32_t*>(geometry.globalTriangles.data()),
+                           reinterpret_cast<uint32_t*>(geometry.globalTriangles.data()),
+                           geometry.globalTriangles.size() * 3, remap.data());
 }
 
 void Scene::buildGeometryBboxes(const ProcessingInfo& processingInfo, GeometryStorage& geometry)
